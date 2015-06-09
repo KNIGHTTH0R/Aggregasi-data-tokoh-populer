8058cff1b41323be78271ca8307a5c4f37805adb
ahmad luky <luky.lucky@yahoo.com>
2015-06-09 20:47:28 +0700

    utilitas OAuth

---
+++ src/utils/OAuthUtils.java
@@ -0,0 +1,18 @@
+/**
+ * define('TWITTER_CONSUMER_KEY','DirV4f3gp3vIUVZDZH5W3k6mT');
+ * define('TWITTER_CONSUMER_SECRET','GYPvZahLYb0JKw1etZlByBEtXZulpQnt8TcQfGtk0d7uFkjw6q');
+ * define('OAUTH_TOKEN','58413351-7sWPSdF3cXvRdq7DwLWfnAgABX6bFJWNpC3LXWUyB');
+ * define('OAUTH_SECRET','fUUwbYJmnDMAtHSLBuYycKzx9RvLKvcKK9Bke1WoqHljo');
+ * define('TWEET_ERROR_INTERVAL',10);
+ * define('TWEET_ERROR_ADDRESS','*****');
+ */
+
+package utils;
+
+public class OAuthUtils
+{
+    public static final String TWITTER_CONSUMER_SECRET  = "GYPvZahLYb0JKw1etZlByBEtXZulpQnt8TcQfGtk0d7uFkjw6q";
+    public static final String TWITTER_CONSUMER_KEY = "DirV4f3gp3vIUVZDZH5W3k6mT";
+    public static final String OAUTH_TOKEN = "7sWPSdF3cXvRdq7DwLWfnAgABX6bFJWNpC3LXWUyB";
+    public static final String OAUTH_SECRET ="fUUwbYJmnDMAtHSLBuYycKzx9RvLKvcKK9Bke1WoqHljo";
+}
