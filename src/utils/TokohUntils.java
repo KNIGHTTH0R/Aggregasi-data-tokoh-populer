c7218d24a7bd6a890c4fda7ca2ae4f398b427e50
ahmad luky <luky.lucky@yahoo.com>
2015-07-02 11:32:16 +0700

    update 2 juli 2015

--- src/utils/TokohUntils.java
+++ src/utils/TokohUntils.java
@@ -10,8 +10,8 @@ package utils;
  * @author ahmadluky
  */
 public class TokohUntils {
+    public static final String LIST_URI = "data/conf/list_uri.conf";
     public static final String NAMA_TOKOH = "data/result/nama_tokoh.txt";
     public static final String RSS_CONFIG = "data/conf/rss.conf";
-    public static final String LIST_URI = "data/conf/list_uri.conf";
     public static final String path_NewSPage = "data/html/";
 }
